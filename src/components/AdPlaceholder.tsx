710593cbc9ddaa1a4254624f39a299c8ce4df858
Atremenyu <91966201+Atremenyu@users.noreply.github.com>
2026-05-15 16:11:45 -0600

    Update AdPlaceholder.tsx

--- src/components/AdPlaceholder.tsx
+++ src/components/AdPlaceholder.tsx
@@ -47,14 +47,14 @@ const AdPlaceholder: React.FC<AdPlaceholderProps> = ({ type, label }) => {
         }
       }}
     >
-      {<!-- Lateral Izquierdo -->
+      <!-- Lateral Izquierdo -->
 <ins class="adsbygoogle"
      style="display:inline-block;width:180px;height:600px"
      data-ad-client="ca-pub-8438097053505351"
      data-ad-slot="7635642545"></ins>
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
-</script>}
+</script>
       <Typography variant="caption" color="text.secondary" sx={{ textAlign: 'center', p: 1 }}>
         Espacio para anuncio {type === 'vertical' ? '160x600' : 'Horizontal'}
         <br />
